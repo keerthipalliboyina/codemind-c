d3a9fe598141b735487d4803991e50ee0cff54c2
keerthipalliboyina <147791828+keerthipalliboyina@users.noreply.github.com>
2023-10-23 12:12:49 +0530

    #include<stdio.h> int main() {     int x,y;     scanf("%d%d%d",&x,&y);     int z=y-x;     if(z=y-x)     {         printf("%d",z);     }     else     {         printf("0");     } }

---
+++ Donation.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    scanf("%d%d%d",&x,&y);
+    int z=y-x;
+    if(z=y-x)
+    {
+        printf("%d",z);
+    }
+    else 
+    {
+        printf("0");
+    }
+}
