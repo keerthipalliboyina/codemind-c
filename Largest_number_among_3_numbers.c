fe00c15abc5b40940d52745f8cbee91783404701
keerthipalliboyina <147791828+keerthipalliboyina@users.noreply.github.com>
2023-10-28 19:14:03 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(a>=b+c)     {         printf("%d",a);     }     else if(b>=a+c)     {         printf("%d",b);     }     else if(c>=a+b)     {         printf("%d",c);     }     else     {         printf("non of the above");     } }

---
+++ Largest_number_among_3_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>=b+c)
+    {
+        printf("%d",a);
+    }
+    else if(b>=a+c)
+    {
+        printf("%d",b);
+    }
+    else if(c>=a+b)
+    {
+        printf("%d",c);
+    }
+    else
+    {
+        printf("non of the above");
+    }
+}
