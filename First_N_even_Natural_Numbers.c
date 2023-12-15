ecfc076f1287b6ac4dc40dc68347b215c6fb19e6
keerthipalliboyina <147791828+keerthipalliboyina@users.noreply.github.com>
2023-12-15 16:12:08 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     for(i=n*2;i>=2;i-=2)     {         if(i%2==0)         {             printf("%d ",i);         }
        }
        
    }

---
+++ First_N_even_Natural_Numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for(i=n*2;i>=2;i-=2)
+    {
+        if(i%2==0)
+        {
+            printf("%d ",i);
+        }
+        
+    }
+    
+}
