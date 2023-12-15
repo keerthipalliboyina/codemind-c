2febf6e13304bc253acd439ae28347072dd51e82
keerthipalliboyina <147791828+keerthipalliboyina@users.noreply.github.com>
2023-12-15 10:58:40 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int odd[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&odd[i]);     }     int sum=0;     for(i=0;i<n;i++)     {         if(odd[i]%2==1)         sum=sum+odd[i];     }     printf("%d",sum); }

---
+++ Odd_Sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int odd[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&odd[i]);
+    }
+    int sum=0;
+    for(i=0;i<n;i++)
+    {
+        if(odd[i]%2==1)
+        sum=sum+odd[i];
+    }
+    printf("%d",sum);
+}
