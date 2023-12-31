743a910b1d424c639f790d9e4cea84a998a0526a
keerthipalliboyina <147791828+keerthipalliboyina@users.noreply.github.com>
2023-12-31 19:23:05 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     float a[n],sum=0,avg;     for(i=0;i<n;i++)     {         scanf("%f",&a[i]);     }     for(i=0;i<n;i++)     {         sum=sum+a[i];         avg=sum/n;     }     printf("%.2f",avg); }

---
+++ Average_of_the_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    float a[n],sum=0,avg;
+    for(i=0;i<n;i++)
+    {
+        scanf("%f",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+        avg=sum/n;
+    }
+    printf("%.2f",avg);
+}
